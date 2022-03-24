3934f3c5c79fb284dc52d9aa56491a27eb49cfe3
Muhammad Saad Atique <msaadatiq@gmail.com>
2022-03-24 15:44:08 -0400

    Fixed the code, changed MPI_Int to MPI_BYTE

--- HW5.c
+++ HW5.c
@@ -13,7 +13,8 @@ void sample_error(int error, char *string)
 
 int main(int argc, char **argv)
 {
-    int myRank, totalRanks, offset; //holds the current rank and total number of ranks
+    int myRank, totalRanks; //holds the current rank and total number of ranks
+    long long offset;
     MPI_File fh;
     MPI_Status status;
     int bufsize, nints;
@@ -22,43 +23,42 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     MPI_Comm_size(MPI_COMM_WORLD, &totalRanks);
-    bufsize = BLOCKSIZE / totalRanks;
-    nints = bufsize / sizeof(int);
-    printf("Bufsize: %d\n", bufsize);
+    // bufsize = BLOCKSIZE / totalRanks;
+    // nints = bufsize / sizeof(int);
+    // printf("Bufsize: %d\n", bufsize);
 
     // create data array of bufsize for each rank
     // Initialize array with 1s
-    int *writeData = calloc(bufsize, sizeof(int));
-    for (int i = 0; i < bufsize; i++)
+    char *writeData = calloc(BLOCKSIZE, sizeof(char));
+    for (int i = 0; i < BLOCKSIZE; i++)
     {
         writeData[i] = 1;
     }
+    MPI_File_open(MPI_COMM_WORLD, "datafile", MPI_MODE_RDWR | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
 
     for (int i = 0; i < 4; i++)
     {
-        MPI_File_open(MPI_COMM_WORLD, "datafile.txt", MPI_MODE_RDWR | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
-        offset = (BLOCKSIZE * i) + (myRank * bufsize);
-        printf("Offset: %d\n", offset);
-        MPI_File_write_at(fh, offset, writeData, bufsize, MPI_INT, &status);
+        offset = (BLOCKSIZE * totalRanks * i) + (myRank * BLOCKSIZE);
+        // printf("Offset: %d\n", offset);
+        result = MPI_File_write_at(fh, offset, writeData, BLOCKSIZE, MPI_BYTE, &status);
         if (result != MPI_SUCCESS)
             sample_error(result, "MPI_File_write_at");
-
-        MPI_File_close(&fh);
     }
+    MPI_File_close(&fh);
 
     MPI_Barrier(MPI_COMM_WORLD);
     // Read Data from the file
 
-    int *readData = calloc(bufsize, sizeof(int));
-
+    char *readData = calloc(BLOCKSIZE, sizeof(char));
+    MPI_File_open(MPI_COMM_WORLD, "datafile", MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
     for (int i = 0; i < 4; i++)
     {
-        MPI_File_open(MPI_COMM_WORLD, "datafile", MPI_MODE_RDWR, MPI_INFO_NULL, &fh);
-        MPI_File_read_at(fh, (BLOCKSIZE * i) + (myRank * bufsize), readData, bufsize, MPI_INT, &status);
-        MPI_File_close(&fh);
+        offset = (BLOCKSIZE * totalRanks * i) + (myRank * BLOCKSIZE);
+        MPI_File_read_at(fh, offset, readData, BLOCKSIZE, MPI_BYTE, &status);
     }
+    MPI_File_close(&fh);
 
-    for (int i = 0; i < bufsize; i++)
+    for (int i = 0; i < BLOCKSIZE; i++)
     {
         printf("MPI RANK: %d Data: %d ", myRank, readData[i]);
     }
